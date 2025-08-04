1a76cf66474fff7de304e583f0699c497e120eb6
vivekgaddam <vivek.gaddam2005@gmail.com>
2025-08-04 21:07:17 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: '*', // Frontend port
+  origin: 'https://nofoodwastechennainngo.onrender.com', // Frontend port
   credentials: true, // Must be true for cookies to be accepted
 }));
 app.use(express.json());
