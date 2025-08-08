e1c5b091d24d62b6776f8b8073b1e442de778e94
Vivek Gaddam <154349504+VivekGaddam@users.noreply.github.com>
2025-08-09 00:41:51 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,9 +2,9 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ allowedRoles, children }) => {
   const user = JSON.parse(localStorage.getItem('user'));
-
+  const token = localStorage.getItem('token'); 
   // Check if user object is missing OR does not have a valid role
-  if (!user || !user.role || !allowedRoles.includes(user.role)) {
+  if (!token) {
     return <Navigate to="/login" />;
   }
 
